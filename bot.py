2bfcc8bb74cd16f0d5ed6888db4102e013c88cf1
Oscar López <oscarlopezmacias6@gmail.com>
2022-05-05 19:46:48 +0200

    sdf

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import telebot
 import sys
 import time
+import paramiko
+import mysql.connector
+from mysql.connector import Error
 
-
+user_input = ""
 TOKEN_BOT = "2013586678:AAF4hFdaWjokjVMUDredntgLTQi2OQWrO24"
 bot = telebot.TeleBot(TOKEN_BOT)
 @bot.message_handler(commands=["hola"])
@@ -13,26 +16,28 @@ def enviar (message):
 
 @bot.message_handler(commands=["start"])
 def enviar_start (message):
-    bot.reply_to(message, "¡Muy bien comencemos! Espere 30 segundos mientras creamos su base de datos...")
+    bot.reply_to(message, "¡Muy bien comencemos! Aplique un nombre a la base de datos y espere 30 segundos mientras creamos su base de datos:" + user_input)
 
 #Base de datos
 #try:
-#    conn = mariadb.connect(
-#        user="riopy",
-#        password="riopy",
-#        host="192.168.21.235",
-#        port=3306,
-#        database="example"
-
-#    )
-#except mariadb.Error as e:
+#    mydb = mysql.connector.connect(
+#        host="",
+#        port="3306",
+#        user="root",
+#        password="root"
+#        )
+#
+#    mycursor = mydb.cursor()
+#
+#    mycursor.execute("CREATE DATABASE " + user_input)
+#
+#
+#except Error as e:
 #    print(f"Error connecting to MariaDB Platform: {e}")
 #    sys.exit(1)
 
 
     time.sleep(15)
     bot.reply_to(message, "Base de datos creada, ya puede empezar a usar nuestra herramienta.")
-
-
 
 bot.polling()
