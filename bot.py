8db6f818b96d6e73ab7a0e0ec1c6505c4a2712e2
Oscar López <oscarlopezmacias6@gmail.com>
2022-05-19 18:58:51 +0200

    v2.1

--- bot.py
+++ bot.py
@@ -5,36 +5,38 @@ import paramiko
 import mysql.connector
 from mysql.connector import Error
 
-username = update.message.chat.username
+
+
 TOKEN_BOT = "2013586678:AAF4hFdaWjokjVMUDredntgLTQi2OQWrO24"
 bot = telebot.TeleBot(TOKEN_BOT)
 @bot.message_handler(commands=["hola"])
-
 def enviar (message):
     bot.reply_to(message, "¡Bienvenido soy RIOPy!, un BOT diseñado para organizar tu PYME a tu gusto. Para empezar escribe /start y nos pondremos manos a la obra.")
+    bot.send_message(message.from_user.id, "Dime el nombre de tu empresa: ");
+    bot.register_next_step_handler(message, get_var);
 
 
 @bot.message_handler(commands=["start"])
 def enviar_start (message):
-    bot.reply_to(message, "¡Muy bien comencemos! Aplique un nombre a la base de datos y espere 30 segundos mientras creamos su base de datos: " + username)
+    bot.reply_to(message, "¡Muy bien comencemos! Aplique un nombre a la base de datos y espere 30 segundos mientras creamos su base de datos: ")
 
 #Base de datos
-    try:
-        mydb = mysql.connector.connect(
-            host="10.152.183.197",
-            port="3306",
-            user="root",
-            password="root"
-            )
-
-        mycursor = mydb.cursor()
-
-        mycursor.execute("CREATE DATABASE " + username)
-
-
-    except Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
+#    try:
+#        mydb = mysql.connector.connect(
+#            host="10.152.183.197",
+#            port="3306",
+#            user="root",
+#            password="root"
+#            )
+#
+#        mycursor = mydb.cursor()
+#
+#        mycursor.execute("CREATE DATABASE " + user_input)
+#
+#
+#    except Error as e:
+#        print(f"Error connecting to MariaDB Platform: {e}")
+#        sys.exit(1)
 
 
     time.sleep(15)
