23531b9daea7f5c60f5e14064560b978e2044c69
Oscar López <oscarlopezmacias6@gmail.com>
2022-05-20 19:50:27 +0200

    v4.6

--- bot.py
+++ bot.py
@@ -9,6 +9,17 @@ from mysql.connector import Error
 
 TOKEN_BOT = "2013586678:AAF4hFdaWjokjVMUDredntgLTQi2OQWrO24"
 bot = telebot.TeleBot(TOKEN_BOT)
+
+try:
+    mydb = mysql.connector.connect(
+        host="10.152.183.197",
+        port="3306",
+        user="root",
+        password="root"
+        )
+
+    mycursor = mydb.cursor()
+
 @bot.message_handler(commands=["start"])
 def enviar (message):
     bot.reply_to(message, "¡Bienvenido soy RIOPy!, un BOT diseñado para organizar tu PYME a tu gusto. Una vez hayas implementado el nombre, escribe /crear y nos pondremos manos a la obra.")
@@ -24,39 +35,33 @@ def enviar_start (message):
     bot.reply_to(message, "¡Muy bien comencemos! Espere 10 segundos mientras creamos su base de datos nombrada: " + var)
 
 #Base de datos
-    try:
-        mydb = mysql.connector.connect(
-            host="10.152.183.197",
-            port="3306",
-            user="root",
-            password="root"
-            )
 
-        mycursor = mydb.cursor()
 
         mycursor.execute("CREATE DATABASE " + var)
 
 
-    except Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
+
 
 
     time.sleep(5)
     bot.reply_to(message, "Base de datos creada, ya puede empezar a usar nuestra herramienta. Usa /departamento")
 
-
-def get_dep (message):
-    global dep;
-    dep = message.text;
+    mycursor.execute("USE " + var)
 
 @bot.message_handler(commands=["departamento"])
 def crear_tabla(message):
     bot.reply_to(message, "Muy bien empezemos.")
     bot.send_message(message.from_user.id, "Muy bien! Ahora dime uno de los departamento que vas a gestionar dentro de tu base de datos: ");
     bot.register_next_step_handler(message, get_dep);
-    mycursor.execute("USE " + var)
+
+def get_dep (message):
+    global dep;
+    dep = message.text;
+
     mycursor.execute("CREATE TABLE " + dep + "(FIRST_NAME CHAR(20))")
 
+except Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
 bot.polling()
